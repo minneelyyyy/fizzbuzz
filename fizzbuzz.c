9aac3d5199c328eb59c63e5c8db0a4a1f4b59d31
minneelyyyy <aprilkahny@gmail.com>
2022-12-09 19:08:08 -0600

    remove useless functions

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -7,18 +7,6 @@ extern ssize_t sys_write(int fd, void* buf, size_t sz);
 __attribute__((__noreturn__))
 extern void sys_exit(int rval);
 
-size_t strlen(const char* str) {
-	register size_t size = 0;
-
-	while (str[size] != 0) size++;
-
-	return size;
-}
-
-void _strcpy(char* dest, const char* src) {
-	while ((*dest++ = *src++));
-}
-
 size_t numdigits(unsigned int n) {
 	size_t count = 0;
 
@@ -35,7 +23,8 @@ size_t utoa(char* dest, unsigned int n) {
 	size_t i = digit_count;
 
 	if (!digit_count) {
-		_strcpy(dest, "0");
+		dest[0] = '0';
+		dest[1] = '\0';
 		return 1;
 	}
 
@@ -62,8 +51,9 @@ void _start() {
 		else if (i % 5 == 0) sys_write(1, "Buzz\n", 5);
 		else {
 			char buffer[32];
-			buffer[utoa(buffer, i)] = '\n';
-			sys_write(1, buffer, strlen(buffer));
+			size_t sz = utoa(buffer, i);
+			buffer[sz] = '\n';
+			sys_write(1, buffer, sz + 1);
 		}
 	}
 
